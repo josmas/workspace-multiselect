0a22812d043ae7311e0327ead3035f1d210a2647
Hollow Man <hollowman@opensuse.org>
2025-06-07 16:36:45 +0800

    [WIP] Support Blockly v12
    Signed-off-by: Hollow Man <hollowman@opensuse.org>

--- src/multiselect_contextmenu.js
+++ src/multiselect_contextmenu.js
@@ -210,7 +210,7 @@ const registerDuplicate = function() {
         });
         dragSelection.clear();
         multiDraggable.clearAll_();
-        Blockly.common.setSelected(null);
+        Blockly.common.setSelected(workspace);
       } else {
         apply(scope.block);
       }
@@ -821,7 +821,7 @@ const registerSelectAll = function() {
         } else {
           Blockly.getSelected().unselect();
         }
-        Blockly.common.setSelected(null);
+        Blockly.common.setSelected(scope.workspace);
         multiDraggable.clearAll_();
         dragSelectionWeakMap.get(scope.workspace).clear();
       }
@@ -1103,7 +1103,7 @@ const registerCommentDuplicate = function() {
         });
         dragSelection.clear();
         multiDraggable.clearAll_();
-        Blockly.common.setSelected(null);
+        Blockly.common.setSelected(workspace);
       } else {
         apply(scope.comment);
       }
--- src/multiselect_shortcut.js
+++ src/multiselect_shortcut.js
@@ -471,7 +471,7 @@ const registerSelectAll = function() {
         } else {
           Blockly.getSelected().unselect();
         }
-        Blockly.common.setSelected(null);
+        Blockly.common.setSelected(workspace);
         multiDraggable.clearAll_();
         dragSelectionWeakMap.get(workspace).clear();
       }
